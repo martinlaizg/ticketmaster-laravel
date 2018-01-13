ea11f48a3e8afce728cfca6bf5b6f59345912895
Martin Laiz <martinlaizg@gmail.com>
2018-01-13 19:34:42 +0100

    Fix messages conflict

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,15 +51,6 @@
 				</a>
 			</div>
 
-<<<<<<< HEAD
-			<div class="collapse navbar-collapse" id="app-navbar-collapse">
-				<!-- Left Side Of Navbar -->
-				<ul class="nav navbar-nav">
-					<li>
-						<a href="{{ url('/home') }}">Home</a>
-					</li>
-				</ul>
-=======
             <div class="collapse navbar-collapse" id="app-navbar-collapse">
                 <!-- Left Side Of Navbar -->
                 <ul class="nav navbar-nav">
@@ -71,7 +62,6 @@
                     </ul>
                 @endif
                 
->>>>>>> master
 
 				<!-- Right Side Of Navbar -->
 				<ul class="nav navbar-nav navbar-right">
@@ -105,21 +95,11 @@
 
 	@yield('content')
 
-<<<<<<< HEAD
-	<!-- JavaScripts -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb"
-	 crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-	 crossorigin="anonymous"></script>
-	{{--
-	<script src="{{ elixir('js/app.js') }}"></script> --}}
-=======
     <!-- JavaScripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 
->>>>>>> master
 </body>
 
 </html>
