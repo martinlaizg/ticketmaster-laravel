df09dbed899b51502303e51911a63aeeb1b415ae
Martin Laiz <martinlaizg@gmail.com>
2018-01-15 15:46:23 +0100

    Añadido enlace de perfil

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,6 +82,9 @@
 
 						<ul class="dropdown-menu" role="menu">
 							<li>
+								<a href="{{ action('UserController@getProfile') }}">Perfil</a>
+							</li>
+							<li>
 								<a href="{{ url('/logout') }}">
 									<i class="fa fa-btn fa-sign-out"></i>Logout</a>
 							</li>
