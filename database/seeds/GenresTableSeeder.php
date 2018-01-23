125e0c62dc87cd67424b04ec618127cf53ee5567
Martin Laiz <martinlaizg@gmail.com>
2018-01-23 16:03:17 +0100

    Cambios en seeders

--- database/seeds/GenresTableSeeder.php
+++ database/seeds/GenresTableSeeder.php
@@ -12,23 +12,111 @@ class GenresTableSeeder extends Seeder
     public function run()
     {
         DB::table('genres')->insert([
-            'name' => 'Rock',
+            'name' => 'Conciertos',
             'category_id' => 1
         ]);
-
         DB::table('genres')->insert([
-            'name' => 'Pop',
+            'name' => 'Dance/Electrónica',
             'category_id' => 1
         ]);
-
         DB::table('genres')->insert([
-            'name' => 'Reggaeton',
+            'name' => 'Flamenco/Rumba',
             'category_id' => 1
         ]);
-
         DB::table('genres')->insert([
-            'name' => 'Salsa',
+            'name' => 'Hard Rock/Metal',
             'category_id' => 1
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Indie/Alternativo',
+            'category_id' => 1
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Jazz/Blues',
+            'category_id' => 1
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Música Clásica',
+            'category_id' => 1
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Pop/Rock',
+            'category_id' => 1
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Rap/Hip-Hop',
+            'category_id' => 1
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Otros-Música',
+            'category_id' => 1
+		]);
+		
+        DB::table('genres')->insert([
+            'name' => 'Ballet/Danza',
+            'category_id' => 2
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Circo',
+            'category_id' => 2
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Drama',
+            'category_id' => 2
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Magia',
+            'category_id' => 2
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Musical',
+            'category_id' => 2
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Ópera',
+            'category_id' => 2
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Otros-Arte y Teatro',
+            'category_id' => 2
+		]);
+		
+        DB::table('genres')->insert([
+            'name' => 'Festival Dance',
+            'category_id' => 3
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Festival de Música',
+            'category_id' => 3
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Otros-Festival',
+            'category_id' => 3
+		]);
+		
+        DB::table('genres')->insert([
+            'name' => 'Baloncesto',
+            'category_id' => 4
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Ciclismo',
+            'category_id' => 4
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Fútbol',
+            'category_id' => 4
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Motor',
+            'category_id' => 4
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Tenis',
+            'category_id' => 4
+        ]);
+        DB::table('genres')->insert([
+            'name' => 'Otros-Deporte',
+            'category_id' => 4
         ]);
     }
 }
