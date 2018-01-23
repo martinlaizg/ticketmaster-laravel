06961828b294ba465234b6cf80972968ea723022
Sergio Conesa Santonja <scs58@gcloud.ua.es>
2018-01-23 22:49:55 +0100

    Añadido enlace para editar y retocado diseño

--- resources/views/edit_user.blade.php
+++ resources/views/edit_user.blade.php
@@ -22,19 +22,21 @@
         <h4> <span class="label label-primary">Apellidos</span></h4>
         {!! Form::text('surname', $user->surname, ['class' => 'field', 'placeholder' => 'Nombre',
             'autofocus' => 'autofocus', 'required' => 'required']) !!}
-
-        <label for="bdate" class="label label-primary">Fecha</label>
-        <input class="form-control" type="date" name="bdate" id="bdate">
         
-        <h4> <span class="label label-primary">Pais</span></h4></div>
+        <h4><label for="bdate" class="label label-primary">Fecha</label></h4>
+        <input type="date" name="bdate" id="bdate">
+        
+        <h4> <span class="label label-primary">Pais</span></h4>
         {!! Form::text('country', $user->country, ['class' => 'field', 'placeholder' => 'Nombre',
             'autofocus' => 'autofocus', 'required' => 'required']) !!}
 
-        <h4> <span class="label label-primary">Ciudad</span></h4></div>
+        <h4> <span class="label label-primary">Ciudad</span></h4>
         {!! Form::text('poblation', $user->poblation, ['class' => 'field', 'placeholder' => 'Nombre',
             'autofocus' => 'autofocus', 'required' => 'required']) !!}
-          
-        {!! Form::button('<span class="glyphicon glyphicon-floppy-saved margenUp"></span> Confirmar',
+
+        <div class="margenUp"></div>
+
+        {!! Form::button('<span class="glyphicon glyphicon-floppy-saved"></span> Confirmar',
              ['class' => 'btn btn-primary btn-block', 'type' => 'submit']) !!}
     </div>
     {!! Form::close() !!}
